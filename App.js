7e5ec26b583e81753261ff8029bfab629a0b26a7
Parth Aggarwal <aparth33@yahoo.co.in>
2024-05-06 01:09:55 +0530

    episode2

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-/**
- * <div id="parent">
- * <div id="child">
- * <h1>This is a h1 tag</h1>
- * <h2>This is a h2 tag</h2>
- * </div>
- * </div>
- */
-
-// const heading=React.createElement("h1",{},"Hello World from React!");
+import React from 'react';
+import  ReactDOM from 'react-dom/client';
 const root=ReactDOM.createRoot(document.getElementById("root"));
-const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"This is a h1 tag"),React.createElement("h2",{},"This is a h2 tag")]))
-console.log(parent)
+const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"This is a h1 tag"),React.createElement("h2",{},"This is a h2 tag")]))
 root.render(parent);
