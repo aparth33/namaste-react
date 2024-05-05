e926e3ffa5198c8f0a2208eec0606c45706326fd
Parth Aggarwal <aparth33@yahoo.co.in>
2024-05-06 05:29:07 +0530

    episode3

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import  ReactDOM from 'react-dom/client';
 const root=ReactDOM.createRoot(document.getElementById("root"));
-const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"This is a h1 tag"),React.createElement("h2",{},"This is a h2 tag")]))
-root.render(parent);
+// const heading=<h1 id='heading' className='heading' tabIndex="5">This is a heading</h1>;
+const Title=()=><h1>This is Title component</h1>
+const HeadingComponent=()=>{
+    return (<div>
+        {Title()}
+        <Title/>
+        <Title></Title>
+    <h1>This is a HeadingComponent</h1>
+    </div>);
+}
+// const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"This is a h1 tag"),React.createElement("h2",{},"This is a h2 tag")]))
+root.render(<HeadingComponent/>);
